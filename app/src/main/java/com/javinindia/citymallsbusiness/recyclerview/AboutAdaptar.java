d60186c523eb332639b3c3a5dc7160bcf28ac827
Ashish <guptaajit2009@gmail.com>
2016-09-26 17:06:28 +0530

    AboutAdaptar

--- app/src/main/java/com/javinindia/citymallsbusiness/recyclerview/AboutAdaptar.java
+++ app/src/main/java/com/javinindia/citymallsbusiness/recyclerview/AboutAdaptar.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,48 +28,66 @@ public class AboutAdaptar extends RecyclerView.Adapter<AboutAdaptar.ViewHolder> 
     Context context;
     MyClickListener myClickListener;
     ArrayList<CountryModel> countryModelArrayList;
+    private String sName, oName, sEmail, sMobileNum, sLandline, sState, sCity, sAddress, mName, mAddress, mLat, mLong, sPic;
+    private String shopCategory, shopSubCategory, country, pincode, rating, openTime, closeTime, distance;
+    private int offerCount;
 
+    public AboutAdaptar(Context context, String storeName, String ownerName, String Email, String MobileNum, String Landline, String State, String City, String storeAddress, String mallName, String mallAddress, String Lat, String Long, String storePic, String shopCat, String shopSubCat, String storeCountry, String storePincode, String storeRating, String storeOpenTime, String storeCloseTime, String storeDistance, int storeOfferCount) {
+        this.context = context;
+        this.country = storeCountry;
+        this.closeTime = storeCloseTime;
+        this.distance = storeDistance;
+        this.mAddress = mallAddress;
+        this.mLat = Lat;
+        this.mName = mallName;
+        this.mLong = Long;
+        this.offerCount = storeOfferCount;
+        this.oName = ownerName;
+        this.openTime = storeOpenTime;
+        this.pincode = storePincode;
+        this.rating = storeRating;
+        this.sAddress = storeAddress;
+        this.sCity = City;
+        this.sEmail = Email;
+        this.shopCategory = shopCat;
+        this.shopSubCategory = shopSubCat;
+        this.sLandline = Landline;
+        this.sMobileNum = MobileNum;
+        this.sName = storeName;
+        this.sPic = storePic;
+        this.sState = State;
+    }
 
-    public AboutAdaptar(List<CountryModel> mCountryModel) {
+  /*  public AboutAdaptar(List<CountryModel> mCountryModel) {
         this.list = mCountryModel;
         this.countryModelArrayList = new ArrayList<>();
         this.countryModelArrayList.addAll(mCountryModel);
-    }
-
-  /*  public AboutAdaptar(Context context, String name, String description, String imgUrl, ArrayList postUrl, String completionStatus, String createDate, String typeWish, String uId) {
-        this.context = context;
-        this.name = name;
-        this.description = description;
-        this.imgUrl = imgUrl;
-        this.postUrl = postUrl;
-        this.completionStatus = completionStatus;
-        this.createDate = createDate;
-        this.typeWish = typeWish;
-        this.uId = uId;
     }*/
 
-   /* public void setData(List<Object> list) {
-        this.list = list;
-    }
 
-    public List<Object> getData() {
-        return list;
-    }
-*/
+    /* public void setData(List<Object> list) {
+         this.list = list;
+     }
+
+     public List<Object> getData() {
+         return list;
+     }
+ */
     public class ViewHolder extends RecyclerView.ViewHolder {
         int holderId;
-       //***********************Recent list
-        public AppCompatTextView txtShopName,txtRating,txtAddress,txtTiming,txtDistance,txtOffers;
+        //***********************Recent list
+        public AppCompatTextView txtShopName, txtRating, txtAddress, txtTiming, txtDistance, txtOffers;
         public RatingBar ratingBar;
         public RelativeLayout rlMain;
         public CheckBox chkImage;
-        public AppCompatButton btnDirection,btnViewOffers;
+        public AppCompatButton btnDirection, btnViewOffers;
         public ImageView imgShopLogo;
 
-       //********************** about header
-       public AppCompatTextView txtShopNameHeader,txtTimingHeader,txtViewsHeader,txtLocationHeader,txtCategoryHeader,txtFaverateCountHeader;
-       public AppCompatButton btnEditProfile;
-       public ImageView imgLogoAbout;
+        //********************** about header
+        public AppCompatTextView txtShopNameHeader, txtTimingHeader, txtViewsHeader, txtLocationHeader, txtCategoryHeader, txtFaverateCountHeader;
+        public AppCompatButton btnEditProfile;
+        public ImageView imgLogoAbout;
+        public RatingBar ratingBarAbout;
 
 
         public ViewHolder(View itemLayoutView, int ViewType) {
@@ -82,19 +101,20 @@ public class AboutAdaptar extends RecyclerView.Adapter<AboutAdaptar.ViewHolder> 
                 txtDistance = (AppCompatTextView) itemLayoutView.findViewById(R.id.txtDistance);
                 txtOffers = (AppCompatTextView) itemLayoutView.findViewById(R.id.txtOffers);
                 ratingBar = (RatingBar) itemLayoutView.findViewById(R.id.ratingBar);
-                rlMain = (RelativeLayout)itemLayoutView.findViewById(R.id.rlMain);
+                rlMain = (RelativeLayout) itemLayoutView.findViewById(R.id.rlMain);
                 chkImage = (CheckBox) itemLayoutView.findViewById(R.id.chkImage);
                /* btnDirection = (AppCompatButton)itemLayoutView.findViewById(R.id.btnDirection);
                 btnViewOffers = (AppCompatButton)itemLayoutView.findViewById(R.id.btnViewOffers);*/
                 holderId = 1;
             } else {
                 txtShopNameHeader = (AppCompatTextView) itemLayoutView.findViewById(R.id.txtShopNameAbout);
-                txtTimingHeader  = (AppCompatTextView)itemLayoutView.findViewById(R.id.txtTimingAbout);
-                txtViewsHeader = (AppCompatTextView)itemLayoutView.findViewById(R.id.txtViewsAbout);
-                txtLocationHeader = (AppCompatTextView)itemLayoutView.findViewById(R.id.txtLocationAbout);
-                txtCategoryHeader = (AppCompatTextView)itemLayoutView.findViewById(R.id.txtCategoryAbout);
-                txtFaverateCountHeader = (AppCompatTextView)itemLayoutView.findViewById(R.id.txtFaverateCountAbout);
-                btnEditProfile = (AppCompatButton)itemLayoutView.findViewById(R.id.btnEditProfile);
+                txtTimingHeader = (AppCompatTextView) itemLayoutView.findViewById(R.id.txtTimingAbout);
+                txtViewsHeader = (AppCompatTextView) itemLayoutView.findViewById(R.id.txtViewsAbout);
+                txtLocationHeader = (AppCompatTextView) itemLayoutView.findViewById(R.id.txtLocationAbout);
+                txtCategoryHeader = (AppCompatTextView) itemLayoutView.findViewById(R.id.txtCategoryAbout);
+                txtFaverateCountHeader = (AppCompatTextView) itemLayoutView.findViewById(R.id.txtFaverateCountAbout);
+                btnEditProfile = (AppCompatButton) itemLayoutView.findViewById(R.id.btnEditProfile);
+                ratingBarAbout = (RatingBar) itemLayoutView.findViewById(R.id.ratingBarAbout);
                 holderId = 0;
             }
         }
@@ -121,9 +141,9 @@ public class AboutAdaptar extends RecyclerView.Adapter<AboutAdaptar.ViewHolder> 
 //        VolleySingleTon volleySingleTon = VolleySingleTon.getInstance(context);
         if (viewHolder.holderId == 1) {
 
-            final CountryModel countryModel = (CountryModel) list.get(position);
+           /* final CountryModel countryModel = (CountryModel) list.get(position - 1);
             String icoCode = countryModel.getisoCode();
-            final String name = countryModel.getName();
+            final String name = countryModel.getName();*/
 
 
             viewHolder.ratingBar.setRating(Float.parseFloat("2.0"));
@@ -150,21 +170,38 @@ public class AboutAdaptar extends RecyclerView.Adapter<AboutAdaptar.ViewHolder> 
             viewHolder.rlMain.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                  //  myClickListener.onItemClick(position, countryModel);
+                    //  myClickListener.onItemClick(position, countryModel);
                 }
             });
 
 
-
         } else {
+
+            viewHolder.txtShopNameHeader.setText(sName);
+
+
+            viewHolder.txtTimingHeader.setText(openTime + " to " + closeTime);
+
+
+            viewHolder.txtLocationHeader.setText(sAddress);
+
+
+            viewHolder.txtCategoryHeader.setText(shopCategory);
+
+
+//            viewHolder.ratingBarAbout.setRating(Float.valueOf(rating));
+
 
         }
     }
 
-    @Override
+  /*  @Override
     public int getItemCount() {
         return list != null ? list.size() + 1 : 1;
-    }
+    }*/
+  public int getItemCount() {
+      return 100;
+  }
 
     @Override
     public int getItemViewType(int position) {
